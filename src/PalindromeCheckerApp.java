b32bae66dbf51fe4e9940669e3a38cbd23db49fc
EVILSHADOW007 <pranjalverma1010@gmail.com>
2026-02-19 09:57:53 +0530

    Base application setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println(" WELCOME TO PALINDROME CHECKER MANAGEMENT SYSTEM");
+        System.out.println("Version: 1.0");
+        System.out.println("System intialized successfully.");
+    }
+}
