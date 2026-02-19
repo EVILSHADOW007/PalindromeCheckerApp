ae0b474a706b2cbc8935031c4db1a7c237e00f01
EVILSHADOW007 <pranjalverma1010@gmail.com>
2026-02-19 10:18:50 +0530

    UC3:Palindrome Check Using String Reverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,19 +1,18 @@
 import java.util.Scanner;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter text: ");
-        String input = sc.nextLine();
-        boolean isPalindrome = true;
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
-        System.out.println("Input text: " + input);
-        System.out.println("Is it a Palindrome? " + isPalindrome);
-
-        sc.close();
+        if (input.equals(reversed)) {
+            System.out.println("Result: The string is a palindrome.");
+        } else {
+            System.out.println("Result: The string is NOT a palindrome.");
+        }
+        scanner.close();
     }
 }
